d4f452086f85c4ee31b18a1265dccb3e8cacd236
lukasschreiber <info@lukasschreiber.com>
2025-05-01 18:56:25 +0200

    still some issues with rerendering of shape orders

--- src/renderer/layers/ShapeLayer.tsx
+++ src/renderer/layers/ShapeLayer.tsx
@@ -7,6 +7,7 @@ export function ShapeLayer() {
     const g = useLayer(0);
     const shapes = useShapeStore((state) => state.shapes.sort((a, b) => (a.order ?? 0) - (b.order ?? 0)));
 
+    // orders do not work after rerendering
     return shapes.map((shape, index) => (
         <React.Fragment key={shape.id}>
             <Shape g={g} shapeId={shape.id} order={index} />
